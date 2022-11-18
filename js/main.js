23df8f32091b6d518555339ed2932917cbf9dbc2
elton <eltonneiferson@outlook.com>
2022-11-18 08:06:07 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,59 +16,58 @@ let hour = currentTime.getHours()
 let minutes = currentTime.getMinutes()
 let seconds = currentTime.getSeconds()
 
-if (month == 0){month = "Janeiro"}
-if (month == 1){month = "Fevereiro"}
-if (month == 2){month = "Março"}
-if (month == 3){month = "Abril"}
-if (month == 4){month = "Maio"}
-if (month == 5){month = "Junho"}
-if (month == 6){month = "Julho"}
-if (month == 7){month = "Agosto"}
-if (month == 8){month = "Setembro"}
-if (month == 9){month = "Outubro"}
-if (month == 10){month = "Novembro"}
-if (month == 11){month = "Dezembro"}
+function writeTime() {
+    if (month == 0){month = "Janeiro"}
+    if (month == 1){month = "Fevereiro"}
+    if (month == 2){month = "Março"}
+    if (month == 3){month = "Abril"}
+    if (month == 4){month = "Maio"}
+    if (month == 5){month = "Junho"}
+    if (month == 6){month = "Julho"}
+    if (month == 7){month = "Agosto"}
+    if (month == 8){month = "Setembro"}
+    if (month == 9){month = "Outubro"}
+    if (month == 10){month = "Novembro"}
+    if (month == 11){month = "Dezembro"}
 
-if (weekDay == 0){weekDay = "Domingo"}
-if (weekDay == 1){weekDay = "Segunda-Feira"}
-if (weekDay == 2){weekDay = "Terça-Feira"}
-if (weekDay == 3){weekDay = "Quarta-Feira"}
-if (weekDay == 4){weekDay = "Quinta-Feira"}
-if (weekDay == 5){weekDay = "Sexta-Feira"}
-if (weekDay == 6){weekDay = "Sábado"}
+    if (weekDay == 0){weekDay = "Domingo"}
+    if (weekDay == 1){weekDay = "Segunda-Feira"}
+    if (weekDay == 2){weekDay = "Terça-Feira"}
+    if (weekDay == 3){weekDay = "Quarta-Feira"}
+    if (weekDay == 4){weekDay = "Quinta-Feira"}
+    if (weekDay == 5){weekDay = "Sexta-Feira"}
+    if (weekDay == 6){weekDay = "Sábado"}
 
-spanWeek.textContent = weekDay
-spanDays.textContent = String(day).padStart(2, '0')
-spanMonth.textContent = month
-spanYear.textContent = year
-spanHour.textContent = String(hour).padStart(2, '0')
-spanMinutes.textContent = String(minutes).padStart(2, '0')
-spanSeconds.textContent = String(seconds).padStart(2, '0')
-
-function countSeconds() {
-    setTimeout(() => {
-        ++seconds
-        if(seconds == 60){
-            seconds = 0
-            ++minutes
-            spanSeconds.textContent = String(seconds).padStart(2, '0')
-            spanMinutes.textContent = String(minutes).padStart(2, '0')
-        }
-
-        if(minutes == 60){
-            minutes = 0
-            ++hour
-            spanMinutes.textContent = String(minutes).padStart(2, '0')
-            spanHour.textContent = String(hour).padStart(2, '0')
-        }
-        spanSeconds.textContent = String(seconds).padStart(2, '0')
-        countSeconds()
-    }, 1000)
+    spanWeek.textContent = weekDay
+    spanDays.textContent = String(day).padStart(2, '0')
+    spanMonth.textContent = month
+    spanYear.textContent = year
+    spanHour.textContent = String(hour).padStart(2, '0')
+    spanMinutes.textContent = String(minutes).padStart(2, '0')
+    spanSeconds.textContent = String(seconds).padStart(2, '0')
 }
+
+setInterval(() => {
+    ++seconds
+    if(seconds == 60){
+        seconds = 0
+        ++minutes
+        spanSeconds.textContent = String(seconds).padStart(2, '0')
+        spanMinutes.textContent = String(minutes).padStart(2, '0')
+    }
+
+    if(minutes == 60){
+        minutes = 0
+        ++hour
+        spanMinutes.textContent = String(minutes).padStart(2, '0')
+        spanHour.textContent = String(hour).padStart(2, '0')
+    }
+    spanSeconds.textContent = String(seconds).padStart(2, '0')
+}, 1000)
 
 buttonDarkMode.addEventListener('click', () => {
     buttonDarkMode.classList.toggle('moveButton')
     main.classList.toggle('whiteMode')
 })
 
-countSeconds()
+writeTime()
